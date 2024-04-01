d477cb42390f4c3498f5c27010f754171bb7da68
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-31 21:37:25 -0400

    chore(pg-pool): remove pg-pool files

--- src/database/schema/functions.sql
+++
@@ -1,8 +0,0 @@
--- Create a trigger function
-CREATE OR REPLACE FUNCTION update_date_updated()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.date_updated = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
