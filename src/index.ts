3553db3f88550e1ed4b6e0636f1d75875d5c32c0
mmraisi <mahmoudmraisi2@gmail.com>
2024-04-01 01:00:31 -0400

    fix(db): destroy connection before shutting down

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { start } from "./app";
+import { db } from "./database/data-srouce";
 
 (async () => {
 	try {
 		await start();
 	} catch (error) {
 		console.error(error);
+		if (db) {
+			await db.destroy();
+		}
 		process.exit(1);
 	}
 })();
